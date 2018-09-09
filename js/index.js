9a28ea3f3dd749ad57b62359a65f7d099130403b
ktanaha <moutoku1984@gmail.com>
2018-09-09 19:50:34 +0900

    todo app

--- js/index.js
+++ js/index.js
@@ -1,6 +1,24 @@
 var app = new Vue({
   el: "#app",
   data: {
-    message: "Hello World!"
+    newItem: "sample",
+    todos: []
+  },
+  methods: {
+    addItem: function(event) {
+      if (this.newItem === "") {
+        return;
+      }
+      var todo = {
+        item: this.newItem,
+        isDone: false
+      };
+
+      this.todos.push(todo);
+      this.newItem = "";
+    },
+    deleteItem: function(index) {
+      this.todos.splice(index, 1);
+    }
   }
 });
